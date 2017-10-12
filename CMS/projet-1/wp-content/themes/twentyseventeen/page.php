3a77ecc9fd9047a2929dda6f1546208c7ce5b1a3
tetrabirk <leyensl@isl-edu.be>
2017-10-12 20:32:41 +0200

    mi cours

--- CMS/projet-1/wp-content/themes/twentyseventeen/page.php
+++ CMS/projet-1/wp-content/themes/twentyseventeen/page.php
@@ -3,9 +3,12 @@
 get_header(); ?>
 
 <?php
+
 while ( have_posts() ) : the_post();
+    global $post;
+    $post_slug = $post->post_name;
     ?>
-    <h1> <?php the_ID();?> </h1>
+    <h1>ceci est un test :  <?php echo $post_slug ?> </h1>
     <?php
 endwhile;
 ?>
