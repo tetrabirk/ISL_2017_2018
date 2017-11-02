3ed477a5008654d3dbf58790affb3182c06d4377
tetrabirk <ludovic.leyens@gmail.com>
2017-11-02 19:49:33 +0100

    pim pam poum, route fonctionnelle, ébauche de la page categorie etc

--- projet/isl_projet/src/AppBundle/Controller/Categories.php
+++ projet/isl_projet/src/AppBundle/Controller/Categories.php
@@ -17,13 +17,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class Categories extends Controller
 {
+
+    //TODO creer un fichier où je mettrais tt les fonction "getqqlchose" pour éviter les répétitions
+
+    public function getCategoriesDeServices($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(CategorieDeServices::class);
+
+        if($id != 0){
+            $data = $repository->findOneBy(
+                array('id'=> $id)
+            );
+        }else {
+            $data = $repository->findAll();
+        }
+        return $data;
+    }
+
     /**
-     * @Route("/categorie/{slug}", defaults ={"slug"=0}, name="categorie")
+     * @Route("/services/{slug}", defaults ={"slug"=0}, name="services")
      */
     public function renderCategories($slug)
     {
-
-        return $this->render('categories.html.twig',array(
+        $categories = $this->getCategoriesDeServices(0);
+        return $this->render('services.html.twig',array(
+            'categories' => $categories,
 
         ));
     }
